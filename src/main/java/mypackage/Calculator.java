171753daa1f5921d9a5cc268098d7b72721ecc69
kumarpravin704 <53420022+kumarpravin704@users.noreply.github.com>
2019-08-30 12:05:24 +0530

    added bug line 11

--- src/main/java/mypackage/Calculator.java
+++ src/main/java/mypackage/Calculator.java
@@ -8,7 +8,7 @@ public class Calculator extends HttpServlet
 {
 	public long addFucn(long first, long second){
 		
-		return first+second;
+		return first++second;
 	}
 	
 	public long subFucn(long first, long second){
